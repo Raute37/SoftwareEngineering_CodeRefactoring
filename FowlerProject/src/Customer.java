c1815cd76c06a0ef11f97d65bd878e7881376f4b
Raute37 <timorautenberg@gmx.de>
2017-05-16 09:51:07 +0200

    - Removing redundant memory variable thisAmount and set the getCharge()-Function directly

--- FowlerProject/src/Customer.java
+++ FowlerProject/src/Customer.java
@@ -28,11 +28,7 @@ class Customer {
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
-            
-            //determine amounts for each line
-            thisAmount = each.getCharge();
             
             // add frequent renter points
             frequentRenterPoints ++;
@@ -42,8 +38,8 @@ class Customer {
                 frequentRenterPoints ++;
             
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
         //add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
